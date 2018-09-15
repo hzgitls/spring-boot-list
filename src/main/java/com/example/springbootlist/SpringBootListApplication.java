dbcf6fc10dae8beb4fb1ae2bb365616bf33a452e
hzgitls <13735560295@163.com>
2018-09-15 14:41:54 +0800

    Initial commit  我也注释

---
+++ src/main/java/com/example/springbootlist/SpringBootListApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootListApplication.class, args);
+	}
+}
